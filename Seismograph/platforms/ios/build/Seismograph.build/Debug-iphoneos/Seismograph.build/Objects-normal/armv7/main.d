925d5e694c4dc3f03b0c2b8c2a7971b5b037df09
nikolay-diyanov <nikolay.diyanov@telerik.com>
2015-05-11 18:48:49 +0300

    seismograph without ahout app

---
+++ Seismograph/platforms/ios/build/Seismograph.build/Debug-iphoneos/Seismograph.build/Objects-normal/armv7/main.d
@@ -0,0 +1,6 @@
+dependencies: \
+  /Users/diyanov/Desktop/Seismograph/platforms/ios/Seismograph/main.m \
+  /Users/diyanov/Desktop/Seismograph/platforms/ios/NativeScript.framework/Headers/NativeScript.h \
+  /Users/diyanov/Desktop/Seismograph/platforms/ios/NativeScript.framework/Headers/TNSRuntime.h \
+  /Users/diyanov/Desktop/Seismograph/platforms/ios/NativeScript.framework/Headers/TNSRuntime+Inspector.h \
+  /Users/diyanov/Desktop/Seismograph/platforms/ios/TNSDebugging.framework/Headers/TNSDebugging.h
